75e293b96c15968cd8bbf3c57ecd6dc9d289fcef
jedi1118 <pobox.uno@gmail.com>
2025-01-08 14:05:00 -0800

    added comment re task store structure

--- src/reducers/taskSlice.js
+++ src/reducers/taskSlice.js
@@ -4,6 +4,19 @@ const today = new Date();
 const month = today.getMonth();// 0 baseds, 0 is January
 const year = today.getFullYear();
 
+// NOTE: alternative way to organize tasks would be normalize tasks.
+// initialState: {
+    // 'dates': {
+    //     '2025-1-10': [ 11, 23, 45, 234],// date key contains ref to task ids
+    //     '2025-2-6': [ 3, 78]
+    // },
+    // 'tasks': [
+    //     { id: 11, desc: 'blah'},// actual task
+    //     { id: 23, desc: 'blah'}, 
+    //     { id: 234, desc: 'blah'}, 
+    // ] 
+// }
+// but, the data is nested stil, so it's no better than existing structure
 const taskSlice = createSlice({
     name: 'tasks',
     initialState: {// add some demo data
